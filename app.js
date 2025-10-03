f1af322f69b16199ada69c61ef6d935e983ff07a
Hamza0990 <hamzaquluyev09@gmail.com>
2025-10-03 13:08:11 +0400

    Js-9-2

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = prompt("Zehmet olmasa 1 ile 1000 arasinda eded daxil edin!");
+
+if (a >= 1 && a <= 1000) {
+    let c = "This Is Scyle"
+    let i = 0;
+    
+    while (i < a) {
+        console.log(c);
+        i++;
+    }
+} else {
+    console.log("Yalnis cavab verdiz 1 ile 1000 arasinda eded daxil edin!!! ");
+}
